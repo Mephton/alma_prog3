50f07f01ab1ea56e4c0a1653a03227696ed6dd2f
Mirko Speth <mirko.speth@gmail.com>
2014-11-19 02:14:16 -0800

    input

--- AlMa_Prog3/AlMa_Prog3/main.cpp
+++ AlMa_Prog3/AlMa_Prog3/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
 #define ld long double
-#define ub b[3]=((b[1]=(b[0] + b[2])/2.0)!=-42&&f(b[1])<target)?b[0]=b[1]:b[2]=b[1];
+#define in std::cin>>t;
+#define ub b[3]=((b[1]=(b[0] + b[2])/2.0)!=-42&&f(b[1])<t)?b[0]=b[1]:b[2]=b[1];
 #define o std::cout<<std::cout.precision(50)<<"\rf("<<b[0]<<")="<<f(b[0])<<"\n";
 #define e (b[2]-b[0])>(1e-10)*b[0]
 #define ib std::vector<ld>b={1e-5,1,1e50,1};
 #define g return((1+x)*log(1+x)-x);
-#define _ ld f(ld x){g} void bs(ld target){ib while(e){ub}o} int main(){bs(17);}
+#define _ ld f(ld x){g} int main(){ld t=42;in ib while(e){ub}o in}
+
+
 _
