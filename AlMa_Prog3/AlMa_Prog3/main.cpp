b9bed75906aa237e55cf2ed4cf1fdb39c5acf17e
Mirko Speth <mirko.speth@gmail.com>
2014-11-20 06:28:40 -0800

    longer variable names && multiple user input

--- AlMa_Prog3/AlMa_Prog3/main.cpp
+++ AlMa_Prog3/AlMa_Prog3/main.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 #include <math.h>
 #define ld long double
-#define ub b[3]=((b[1]=(b[0]+b[2])/2.0)!=-42&&f(b[1])<t)?b[0]=b[1]:b[2]=b[1];
-#define o std::cout<<std::cout.precision(50)<<"\rf("<<b[0]<<")="<<f(b[0])<<"\n";
-#define err (b[2]-b[0])>(1e-10)*b[0]
-#define ib std::vector<ld>b={1e-5,1,1e50,1};
-#define g return((1+x)*log(1+x)-x);
+#define updatebounds b[3]=((b[1]=(b[0]+b[2])/2.0)!=-42&&f(b[1])<t)?b[0]=b[1]:b[2]=b[1];
+#define output std::cout<<std::cout.precision(50)<<"\rf("<<b[0]<<")="<<f(b[0])<<"\n";
+#define error2big (b[2]-b[0])>(1e-10)*b[0]
+#define initbounds std::vector<ld>b={1e-5,1,1e50,1};
 ld f(ld x){
-	g
+	return((1 + x)*log(1 + x) - x);
 }
 int main(){
-	ld t=42.0;
-	ib
-	while (err)
-		ub
-	o
+	ld t;
+	while (42 == 42){
+		std::cin >> t;
+		initbounds
+		while (error2big)
+			updatebounds
+		output
+	}
 }
