3a697ce99f1e9d502be9bb2f33418831835816c2
Julian Doerfler <jujuedv@gmail.com>
2014-11-22 02:59:55 +0100

    my second version: Size 232 Bytes

--- AlMa_Prog3/AlMa_Prog3/main.cpp
+++ AlMa_Prog3/AlMa_Prog3/main.cpp
@@ -1,3 +1,3 @@
 #include<cstdio>
 #include<cmath>
-typedef long double ld;ld f(ld x){return((1+x)*log(1+x)-x);}main(){for(;;){ld y,u=1e50,l=1/u,m=scanf("%Lf",&y);for(;m=l/2+u/2,u-l>l/1e10;f(m)<y?l=m:u=m);printf("%.50Lf %.50Lf -> %.50Lf\n",l,u,f(m));}}
+typedef long double ld;ld f(ld x){return((1+x)*log(1+x)-x);}main(){for(;;)for(ld y,u=1e50,l=1/u,m=scanf("%Lf",&y);m=l/2+u/2,u-l>l/1e10||!printf("%.50Lf %.50Lf -> %.50Lf\n",l,u,f(m));f(m)<y?l=m:u=m);}
