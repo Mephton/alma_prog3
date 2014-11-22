e29b356b882a0ff651d9e3c1a1fba82d9c3e136e
Julian Doerfler <jujuedv@gmail.com>
2014-11-22 23:12:32 +0100

    ld -> d Size: 228

--- AlMa_Prog3/AlMa_Prog3/main.cpp
+++ AlMa_Prog3/AlMa_Prog3/main.cpp
@@ -1,3 +1,3 @@
 #include<cstdio>
 #include<cmath>
-typedef long double ld;ld f(ld x){return((1+x)*log(1+x)-x);}main(){for(;;)for(ld y,u=1e50,l=1/u,m=scanf("%Lf",&y);m=l/2+u/2,u-l>l/1e10||!printf("%.50Lf %.50Lf -> %.50Lf\n",l,u,f(m));f(m)<y?l=m:u=m);}
+typedef long double d;d f(d x){return((1+x)*log(1+x)-x);}main(){for(;;)for(d y,u=1e50,l=1/u,m=scanf("%Lf",&y);m=l/2+u/2,u-l>l/1e10||!printf("%.50Lf %.50Lf -> %.50Lf\n",l,u,f(m));f(m)<y?l=m:u=m);}
